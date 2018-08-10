bdfb5afb950591b44431aaf7621fd7ce4e0b683d
Nicola Erario <nicola.erario@gmail.com>
2018-08-10 11:59:44 +0200

    Setto il bot; gli insegno le prime parole; inizio la conversazione

--- Pita_ChatBot/Pita_ChatBot.py
+++ Pita_ChatBot/Pita_ChatBot.py
@@ -1,3 +1,32 @@
 from chatterbot import ChatBot
-bot = ChatBot('Pita')
+from chatterbot.trainers import ListTrainer
 
+bot = ChatBot(
+    'Pita', #nome del bot,unico parametro obbligatorio
+    storage_adapter = 'chatterbot.storage.SQLStorageAdapter',
+    database = './pita_db.sqlite3',
+    input_adapter = 'chatterbot.input.VariableInputTypeAdapter',
+    output_adapter = 'chatterbot.output.OutputAdapter',
+    logic_adapters=[
+        {
+            "import_path": "chatterbot.logic.BestMatch",
+            "statement_comparison_function": "chatterbot.comparisons.levenshtein_distance",
+            "response_selection_method": "chatterbot.response_selection.get_first_response"
+            }
+        ],
+    )
+#apro il file e lo setto come trainer del bot
+with open ('./prima_conversazione.txt') as f:
+    conversation = f.readlines()
+    bot.set_trainer(ListTrainer)
+    bot.train(conversation)
+
+#avvio la conversazione in ciclo infinito
+while True:
+    try:
+        user_input = input('Msg: ')
+        response = bot.get_response(user_input)
+        print('Pita: ', response)
+    except(KeyboardInterrupt, SystemExit):
+        print('Arrivederci!')
+        break
